f2df51f43ce78f854c1e1353c1b84b9b02806d28
K00318993 <AnasyKabundi00@gmail.com>
2026-01-30 11:46:45 +0000

    1st Commit of lab 3

---
+++ src/main/java/com/example/demo/domain/Book.java
@@ -0,0 +1,62 @@
+package com.example.demo.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import org.apache.tomcat.jni.Library;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //@NotBlank(message = "Book title must not be empty")
+    private String title;
+    private String author;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "library_id")
+    @JsonBackReference
+    private Library library;
+
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book() {
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Library getLibrary() {
+        return library;
+    }
+
+    public void setLibrary(Library library) {
+        this.library = library;
+    }
+}
